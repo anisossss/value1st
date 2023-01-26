95dfb44e1333b3d5174daac1fb9fa0316f7ec2f7
anis khalef <anis.khalef@duetodata.com>
2023-01-26 09:10:51 +0100

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,43 @@
-import '../styles/globals.css';
-import type {AppProps} from 'next/app';
-import {createTheme, NextUIProvider} from '@nextui-org/react';
-import {ThemeProvider as NextThemesProvider} from 'next-themes';
-import { Layout } from '@/components/navbar/layout';
-import { SSRProvider } from "@react-aria/ssr"; 
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Layout } from "@/components/navbar/layout";
+import { SSRProvider } from "@react-aria/ssr";
 
 const lightTheme = createTheme({
-   type: 'light',
-   theme: {
-      colors: {},
-   },
+  type: "light",
+  theme: {
+    colors: {},
+  },
 });
 
 const darkTheme = createTheme({
-   type: 'dark',
-   theme: {
-      colors: {},
-   },
+  type: "dark",
+  theme: {
+    colors: {},
+  },
 });
 
-function MyApp({Component, pageProps}: AppProps) {
-   return (
-     <NextThemesProvider
-       defaultTheme="system"
-       attribute="class"
-       value={{
-         light: lightTheme.className,
-         dark: darkTheme.className,
-       }}
-     >
-             <SSRProvider>
-
-       <Layout>
-         <NextUIProvider>
-           <Component {...pageProps} />
-         </NextUIProvider>
-         </Layout>
-         </SSRProvider>
-     </NextThemesProvider>
-   );
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <SSRProvider>
+        <Layout>
+          <NextUIProvider>
+            <Component {...pageProps} />
+          </NextUIProvider>
+        </Layout>
+      </SSRProvider>
+    </NextThemesProvider>
+  );
 }
 
 export default MyApp;
